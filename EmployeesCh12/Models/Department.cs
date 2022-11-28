59a060947c77682977587ebff5aef5e39f01070f
rphinizy <robinphinizy@yahoo.com>
2022-11-27 22:10:10 -0500

    Add project files.

---
+++ EmployeesCh12/Models/Department.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Collections.Generic;
+
+namespace EmployeesCh12.Models
+{
+    public class Department
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [StringLength(50)]
+        [Display(Name = "Department")]
+        public string Name { get; set; }
+
+        // Navgation property
+        public virtual ICollection<Employee> Employees { get; set; }
+
+        // Navigation Property
+
+        public ICollection<DepartmentLocation> DepartmentLocations { get; set; }
+    }
+}
---
+++ EmployeesCh12/Models/DepartmentLocation.cs
@@ -0,0 +1,14 @@
+﻿namespace EmployeesCh12.Models
+{
+    public class DepartmentLocation
+    {
+        //Composit Primary Ket and foreign key for Department
+        public int DepartmentID { get; set; }
+        //composite primary key and foreign key for location
+        public int LocationID { get; set; }
+
+        //nagivation properties
+        public Department Department { get; set; }
+        public Location Location { get; set; }
+    }
+}
